906d3b5eb5775ef339aef3cf62fba70e2301c656
Bryan Lee <bryanrichie97@gmail.com>
2021-01-27 13:58:08 +1100

    feat(app): added image url submit functionality, conditional image rendering and error validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,72 @@
-import { AspectRatio, Box, Button, Flex, Heading, Image } from '@chakra-ui/react';
-import React from 'react';
+import {
+  AspectRatio,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  Input,
+  useToast,
+} from '@chakra-ui/react';
+import React, { useState } from 'react';
+import _ from 'lodash';
 
-function App() {
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [img, setImg] = useState('');
+
+  const toast = useToast();
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+
+    if (_.includes(query, 'jpg' || 'png')) {
+      setImg(query);
+      setQuery('');
+    } else {
+      toast({
+        title: 'Invalid image url',
+        status: 'error',
+        position: 'top',
+        duration: 3000,
+      });
+      setImg('');
+      setQuery('');
+    }
+  };
+
   return (
     <Flex flexDirection="column" alignItems="center" bgColor="green.500" minHeight="100vh">
       <Heading as="h1" color="white">
         Image Flipper
       </Heading>
+      <Flex justifyContent="center">
+        <form onSubmit={onSubmit}>
+          <HStack spacing={0}>
+            <Input
+              w={412}
+              placeholder="Enter image URL..."
+              bgColor="white"
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+            />
+            <Button type="submit">Submit</Button>
+          </HStack>
+        </form>
+      </Flex>
       <Flex my={7.5}>
         <Button>Flip Vertical</Button>
         <Button>Flip Horizontal</Button>
         <Button>Rotate 90°</Button>
         <Button>Rotate 180°</Button>
       </Flex>
-      <AspectRatio width="100%" maxW="500px" ratio={1}>
-        <Image src="rushia.jpg" alt="rushia" />
-      </AspectRatio>
+      {img ? (
+        <AspectRatio width="100%" maxW="500px" ratio={1}>
+          <Image src={img} />
+        </AspectRatio>
+      ) : null}
     </Flex>
   );
-}
+};
 
 export default App;

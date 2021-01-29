65d655735171659751a4cf996eb6e670e8c8fd74
Bryan Lee <bryanrichie97@gmail.com>
2021-01-29 13:31:28 +1100

    feat(app): added button functionality and more icon and styling on buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import {
-  AspectRatio,
-  Button,
-  Flex,
-  Heading,
-  HStack,
-  Image,
-  Input,
-  useToast,
-} from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, HStack, Image, Input, useToast } from '@chakra-ui/react';
+import { AiOutlineRotateLeft, AiOutlineRotateRight } from 'react-icons/ai';
+import { GiHorizontalFlip, GiVerticalFlip } from 'react-icons/gi';
+import { BiReset } from 'react-icons/bi';
+import { MdCropRotate } from 'react-icons/md';
 import React, { useState } from 'react';
-import _ from 'lodash';
 
 export const App = () => {
   const [query, setQuery] = useState('');
   const [img, setImg] = useState('');
+  const [transform, setTransform] = useState('');
 
   const toast = useToast();
 
@@ -50,19 +45,33 @@ export const App = () => {
               value={query}
             />
             <Button type="submit">Submit</Button>
+            <Button rightIcon={<BiReset />} onClick={() => setTransform('')}>
+              Reset
+            </Button>
           </HStack>
         </form>
       </Flex>
       <Flex my={7.5}>
-        <Button>Flip Vertical</Button>
-        <Button>Flip Horizontal</Button>
-        <Button>Rotate 90°</Button>
-        <Button>Rotate 180°</Button>
+        <Button rightIcon={<GiVerticalFlip />} onClick={() => setTransform('scaleY(-1)')}>
+          Flip Vertical
+        </Button>
+        <Button rightIcon={<GiHorizontalFlip />} onClick={() => setTransform('scaleX(-1)')}>
+          Flip Horizontal
+        </Button>
+        <Button rightIcon={<AiOutlineRotateLeft />} onClick={() => setTransform('rotate(-90deg)')}>
+          90°
+        </Button>
+        <Button rightIcon={<AiOutlineRotateRight />} onClick={() => setTransform('rotate(90deg)')}>
+          90°
+        </Button>
+        <Button rightIcon={<MdCropRotate />} onClick={() => setTransform('rotate(180deg)')}>
+          Rotate 180°
+        </Button>
       </Flex>
       {img ? (
-        <AspectRatio width="100%" maxW="500px" ratio={1}>
-          <Image src={img} onError={onError} />
-        </AspectRatio>
+        <Box transform={`${transform}`}>
+          <Image className="image" src={img} boxSize={750} onError={onError} fit="scale-down" />
+        </Box>
       ) : null}
     </Flex>
   );
